2f83fa147816f49888430b822fb5eb538c4c351b
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-21 17:28:24 -0500

    Troubleshot Database.php port error.

--- config/Database.php
+++ config/Database.php
@@ -10,11 +10,12 @@
             // Environment Variables
             $host = isset($_ENV['INF653_HOST']) ? $_ENV['INF653_HOST'] : '';
             $db_name = isset($_ENV['INF653_DB']) ? $_ENV['INF653_DB'] : '';
+            $db_port = isset($_ENV['INF653_PORT']) ? $_ENV['INF653_PORT'] : '';
             $username = isset($_ENV['INF653_USER']) ? $_ENV['INF653_USER'] : '';
             $password = isset($_ENV['INF653_PW']) ? $_ENV['INF653_PW'] : '';
 
             try {
-                $this->conn = new PDO('psql:host=' . $host . ';dbname=' . $db_name, $username, $password);
+                $this->conn = new PDO('pgsql:host=' . $host . ';port=' . $db_port . ';dbname=' . $db_name, $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
