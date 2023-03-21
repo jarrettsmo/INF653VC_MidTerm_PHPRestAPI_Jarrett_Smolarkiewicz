dfce65dabfb5f95ef0747572ae99c96f93e99d60
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-21 17:13:02 -0500

    Troubleshot Access Control Header.

--- inc.php
+++ inc.php
@@ -3,14 +3,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-    header('Access-Control-Allow-Headers:   Access-Control-Allow-Headers,
-                                            Origin,
-                                            Accept,
-                                            Content-Type, 
-                                            Access-Control-Allow-Methods, 
-                                            Authorization, 
-                                            X-Requested-With'
-    );
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin, Accept, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once 'config/Database.php';
     include_once 'models/Author.php';
